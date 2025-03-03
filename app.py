ba99c9ac870e26b7fed5dcdb561523fdd94a4d30
rtasouji <r.tasouji@gmail.com>
2025-03-03 17:00:45 +0000

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ import os
 import plotly.graph_objects as go
 import logging
 import sys
-logger.info(f"Current working directory: {os.getcwd()}")
 # Configure logging
 logging.basicConfig(
     level=logging.INFO,
